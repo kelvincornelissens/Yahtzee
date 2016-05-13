f8ff535848f9e898cb03707b260d47f4b577c4d1
kelvincornelissens <kelvincornelissens@hotmail.com>
2016-05-13 12:28:08 +0200

    Count functies voor buttons

--- js/custom.js
+++ js/custom.js
@@ -3,56 +3,125 @@ Count = {
         var Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 1){
-                Score+=1;
-                document.getElementById("aces").innerHTML = Score;
-                return Score;
+                Score++;
             }
+
         }
+        document.getElementById("aces").innerHTML = Score;
+        return Score;
     },
     CountTwo: function (DiceArray) {
         Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 2){
                 Score+=2;
-                return Score;
+
             }
         }
+        document.getElementById("twos").innerHTML = Score;
+        return Score;
     },
     CountThree: function (DiceArray) {
         Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 3){
                 Score+=3;
-                return Score;
+
             }
         }
+        document.getElementById("threes").innerHTML = Score;
+        return Score;
     },
     CountFour: function (DiceArray) {
         Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 4){
                 Score+=4;
-                return Score;
+
             }
         }
+        document.getElementById("fours").innerHTML = Score;
+        return Score;
     },
     CountFive: function (DiceArray) {
         Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 5){
                 Score+=5;
-                return Score;
             }
         }
+        document.getElementById("fives").innerHTML = Score;
+        return Score;
     },
     CountSix: function (DiceArray) {
         Score = 0;
         for(i = 0; i < DiceArray.length; i++){
             if(DiceArray[i].value === 6){
                 Score+=6;
-                return Score;
             }
         }
+        document.getElementById("sixes").innerHTML = Score;
+        return Score;
+    },
+    CountThreeOfAKind: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidThreeOfAKind(Validator.diceArrayToIntArray(DiceArray))){
+            for(i = 0; i < DiceArray.length; i++){
+                Score += DiceArray[i].value;
+            }
+        }
+        document.getElementById("threekind").innerHTML = Score;
+        return Score;
+    },
+    CountFourOfAKind: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidFourOfAKind(Validator.diceArrayToIntArray(DiceArray))){
+            for(i = 0; i < DiceArray.length; i++){
+                Score += DiceArray[i].value;
+            }
+        }
+        document.getElementById("fourkind").innerHTML = Score;
+        return Score;
+    },
+    CountFullHouse: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidFullHouse(Validator.diceArrayToIntArray(DiceArray))){
+            Score = 25;
+        }
+        document.getElementById("fullhouse").innerHTML = Score;
+        return Score;
+    },
+    CountSmallStraight: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidSmallStraight(Validator.diceArrayToIntArray(DiceArray))){
+            Score = 30;
+        }
+        document.getElementById("smallStraight").innerHTML = Score;
+        return Score;
+    },
+    CountLargeStraight: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidLargeStraight(Validator.diceArrayToIntArray(DiceArray))){
+            Score = 40;
+        }
+        document.getElementById("smallStraight").innerHTML = Score;
+        return Score;
+    },
+    CountYahtzee: function (DiceArray) {
+        Score = 0;
+        if(Validator.isValidYahtzee(Validator.diceArrayToIntArray(DiceArray))){
+            Score = 50;
+        }
+        document.getElementById("yahtzee").innerHTML = Score;
+        return Score;
+    },
+    CountChance: function (DiceArray) {
+        Score = 0;
+        for(i = 0; i < DiceArray.length; i++){
+            Score += DiceArray[i].value;
+        }
+        document.getElementById("chance").innerHTML = Score;
+        return Score;
     }
 };
 
@@ -80,7 +149,7 @@ valueTotalUpper = valueAce + valueTwos + valueThrees + valueFours + valueFives +
 valueTotal = valueTotalUpper + valueThreeKind + valueFourKind + valueFullHouse + valueSmallStr + valueLargeStr + valueChance + valueYahtzee;
 
 // Zet score in tabel
-document.getElementById("aces").innerHTML = valueAce;
+/*document.getElementById("aces").innerHTML = valueAce;
 document.getElementById("twos").innerHTML = valueTwos;
 document.getElementById("threes").innerHTML = valueThrees;
 document.getElementById("fours").innerHTML = valueFours;
@@ -95,4 +164,4 @@ document.getElementById("chance").innerHTML = valueChance;
 document.getElementById("yahtzeeScore").innerHTML = valueYahtzee;
 document.getElementById("totalUpper").innerHTML = valueTotalUpper;
 document.getElementById("bonus").innerHTML = valueBonus;
-document.getElementById("total").innerHTML = valueTotal;
+document.getElementById("total").innerHTML = valueTotal;*/
