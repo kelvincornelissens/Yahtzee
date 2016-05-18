2a9df8a8c9c6918ef9a11ff8416694398c59c838
kelvincornelissens <kelvincornelissens@hotmail.com>
2016-05-18 08:41:24 +0200

    Foutje in id, komt door carlo

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,7 @@ Count = {
         if(Validator.isValidLargeStraight(Validator.diceArrayToIntArray(DiceArray))){
             Score = 40;
         }
-        document.getElementById("smallStraight").innerHTML = Score;
+        document.getElementById("largeStraight").innerHTML = Score;
         return Score;
     },
     CountYahtzee: function (DiceArray) {
@@ -112,7 +112,7 @@ Count = {
         if(Validator.isValidYahtzee(Validator.diceArrayToIntArray(DiceArray))){
             Score = 50;
         }
-        document.getElementById("yahtzee").innerHTML = Score;
+        document.getElementById("yahtzeeScore").innerHTML = Score;
         return Score;
     },
     CountChance: function (DiceArray) {
@@ -128,40 +128,3 @@ Count = {
 
 
 
-valueAce = 10;
-valueTwos = 20;
-valueThrees = 30;
-valueFours = 40;
-valueFives = 50;
-valueSixes = 60;
-subtotal = valueAce + valueTwos + valueThrees + valueFours + valueFives + valueSixes;
-valueBonus = 0;
-if (subtotal > 63) valueBonus = 30;
-
-valueThreeKind = 30;
-valueFourKind = 40;
-valueFullHouse = 25;
-valueSmallStr = 30;
-valueLargeStr = 40;
-valueChance = 50;
-valueYahtzee = 60;
-valueTotalUpper = valueAce + valueTwos + valueThrees + valueFours + valueFives + valueSixes + valueBonus;
-valueTotal = valueTotalUpper + valueThreeKind + valueFourKind + valueFullHouse + valueSmallStr + valueLargeStr + valueChance + valueYahtzee;
-
-// Zet score in tabel
-/*document.getElementById("aces").innerHTML = valueAce;
-document.getElementById("twos").innerHTML = valueTwos;
-document.getElementById("threes").innerHTML = valueThrees;
-document.getElementById("fours").innerHTML = valueFours;
-document.getElementById("fives").innerHTML = valueFives;
-document.getElementById("sixes").innerHTML = valueSixes;
-document.getElementById("threekind").innerHTML = valueThreeKind;
-document.getElementById("fourkind").innerHTML = valueFourKind;
-document.getElementById("fullhouse").innerHTML = valueFullHouse;
-document.getElementById("smallStraight").innerHTML = valueSmallStr;
-document.getElementById("largeStraight").innerHTML = valueLargeStr;
-document.getElementById("chance").innerHTML = valueChance;
-document.getElementById("yahtzeeScore").innerHTML = valueYahtzee;
-document.getElementById("totalUpper").innerHTML = valueTotalUpper;
-document.getElementById("bonus").innerHTML = valueBonus;
-document.getElementById("total").innerHTML = valueTotal;*/
